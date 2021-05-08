55fc0acedf18a780e1fac53a59b5a9be07902fc4
nqounet <mail@nqou.net>
2021-05-08 11:05:01 +0900

    add project files from start.spring.io(main)

---
+++ src/main/kotlin/net/nqou/darkinsane/DarkInsaneApplication.kt
@@ -0,0 +1,11 @@
+package net.nqou.darkinsane
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DarkInsaneApplication
+
+fun main(args: Array<String>) {
+	runApplication<DarkInsaneApplication>(*args)
+}
---
+++ src/test/kotlin/net/nqou/darkinsane/DarkInsaneApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.nqou.darkinsane
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DarkInsaneApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
